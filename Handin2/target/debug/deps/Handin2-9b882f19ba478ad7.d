9dce7fc45c06c8d02fc6bf914cf0f65a96280091
ChristofferCJ <christofferchangtrakuljensen@gmail.com>
2022-10-09 11:10:26 +0200

    Cleanup

--- Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.d
+++ Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.d
@@ -1,9 +1,13 @@
-/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.rmeta: src/main.rs src/graham.rs src/gift_wrapping.rs src/chan.rs src/structs.rs
+/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.rmeta: src/main.rs src/app.rs src/app/graham.rs src/app/gift_wrapping.rs src/app/chan.rs src/app/structs.rs src/tests.rs src/tests/test.rs src/tests/test_data.rs
 
-/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.d: src/main.rs src/graham.rs src/gift_wrapping.rs src/chan.rs src/structs.rs
+/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-9b882f19ba478ad7.d: src/main.rs src/app.rs src/app/graham.rs src/app/gift_wrapping.rs src/app/chan.rs src/app/structs.rs src/tests.rs src/tests/test.rs src/tests/test_data.rs
 
 src/main.rs:
-src/graham.rs:
-src/gift_wrapping.rs:
-src/chan.rs:
-src/structs.rs:
+src/app.rs:
+src/app/graham.rs:
+src/app/gift_wrapping.rs:
+src/app/chan.rs:
+src/app/structs.rs:
+src/tests.rs:
+src/tests/test.rs:
+src/tests/test_data.rs:
