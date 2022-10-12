d545f2ecc32087fcd85db681cb6a49c30f0cafba
Milosz Glowaczewski <milosz.glowaczewski@gmail.com>
2022-10-12 11:07:36 +0200

    gitignore fix

--- Handin2/target/debug/deps/Handin2-4a93ed16bc4b243f.d
+++
@@ -1,8 +0,0 @@
-/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-4a93ed16bc4b243f: src/main.rs src/graham.rs src/gift_wrapping.rs src/structs.rs
-
-/Users/changtrakul/code/CompGeo/Handin2/target/debug/deps/Handin2-4a93ed16bc4b243f.d: src/main.rs src/graham.rs src/gift_wrapping.rs src/structs.rs
-
-src/main.rs:
-src/graham.rs:
-src/gift_wrapping.rs:
-src/structs.rs:
